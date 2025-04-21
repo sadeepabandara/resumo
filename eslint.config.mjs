f5e12a64a4fb152c2a5f552f63ca5a5ec189c558
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-04-21 10:50:52 +1000

    setup the project and add shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+    ...compat.config({
+        extends: [
+            "next/core-web-vitals",
+            "next/typescript",
+            "prettier",
+            "plugin:tailwindcss/recommended",
+        ],
+        plugins: ["prettier"],
+        rules: {
+            "prettier/prettier": "error",
+            "react/no-escape-entities": "off",
+        },
+    }),
+};
 
 export default eslintConfig;
